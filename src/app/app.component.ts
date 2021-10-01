1c51237ef94b195aad387c0e3b6b1070a8f07763
Felipe Manrique <amanrique10@gmail.com>
2021-10-01 10:36:14 -0500

    add GTM to  project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 import { AuthService } from './auth';
 import { User } from './models';
@@ -7,7 +8,8 @@ import { User } from './models';
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    providers: [GoogleTagManagerService]
 })
 export class AppComponent implements OnInit {
     btnActionLogout:string = 'logout';
@@ -19,7 +21,8 @@ export class AppComponent implements OnInit {
     constructor(
         private authService: AuthService,
         private router: Router,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private gtmService: GoogleTagManagerService,
     ) {
         this.authService.user.subscribe(res => this.user = res);
      }
@@ -27,6 +30,18 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         //TODO: Needs to be refactored
         this.btnActionLogin = (window.location.pathname !== '/account/register') ? 'sign up' : 'login';
+        this.gtmService.addGtmToDom();
+
+        this.router.events.forEach(item => {
+            if (item instanceof NavigationEnd) {
+                const gtmTag = {
+                    event: 'page',
+                    pageName: item.url
+                };
+
+                this.gtmService.pushTag(gtmTag);
+            }
+        });
     }
 
     register(){
