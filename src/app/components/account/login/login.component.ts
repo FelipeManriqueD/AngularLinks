b2b547e6d4e7c32c023a4c55f74bb70cfa1bf455
Felipe Manrique <amanrique10@gmail.com>
2021-10-06 16:31:53 -0500

    strat layout changes

--- src/app/components/account/login/login.component.ts
+++ src/app/components/account/login/login.component.ts
@@ -7,58 +7,58 @@ import { AuthService } from '../../../auth';
 
 
 @Component({
-    templateUrl: 'login.component.html',
-    styleUrls: ['../account.component.scss']
+	templateUrl: 'login.component.html',
+	styleUrls: ['login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    emailText = 'Your Email';
-    PasswordText = 'Password';
-    loginForm: FormGroup;
-    submitted = false;
-    returnUrl: string;
+	emailText = 'Your Email';
+	PasswordText = 'Password';
+	loginForm: FormGroup;
+	submitted = false;
+	returnUrl: string;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authService: AuthService
-    ) {
-        //Go home if user logged
-        if(this.authService.userVal) {
-            this.router.navigate(['/'])
-        }
-    }
+	constructor(
+		private formBuilder: FormBuilder,
+		private route: ActivatedRoute,
+		private router: Router,
+		private authService: AuthService
+	) {
+		//Go home if user logged
+		if (this.authService.userVal) {
+			this.router.navigate(['/'])
+		}
+	}
 
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            email: ['', Validators.required],
-            password: ['', Validators.required]
-        });
+	ngOnInit() {
+		this.loginForm = this.formBuilder.group({
+			email: ['', Validators.required],
+			password: ['', Validators.required]
+		});
 
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
+		this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+	}
 
-    get form(){
-        return this.loginForm.controls
-    }
+	get form() {
+		return this.loginForm.controls
+	}
 
-    onSubmit() {
-        this.submitted = true;
+	onSubmit() {
+		this.submitted = true;
 
-        if(this.loginForm.invalid){
-            return
-        }
+		if (this.loginForm.invalid) {
+			return
+		}
 
-        const {email, password} = this.form;
-        const emailVal = email.value;
-        const passwordvalue = password.value;
+		const { email, password } = this.form;
+		const emailVal = email.value;
+		const passwordvalue = password.value;
 
-        this.authService.login(emailVal, passwordvalue)
-            .subscribe(data => {
-                if(typeof data !== 'undefined') {
-                    localStorage.setItem('user', JSON.stringify(data))
-                    this.router.navigate([this.returnUrl])
-                }
-            })
-    }
+		this.authService.login(emailVal, passwordvalue)
+			.subscribe(data => {
+				if (typeof data !== 'undefined') {
+					localStorage.setItem('user', JSON.stringify(data))
+					this.router.navigate([this.returnUrl])
+				}
+			})
+	}
 }
